40cf4825539bdca578f7a31e344d133e06f3f74f
SerhiiNahradskyi <djseryoga3@gmail.com>
2024-10-14 14:05:23 +0200

    homework

---
+++ main.py
@@ -0,0 +1,70 @@
+# print("Hello world!")
+#
+#
+
+
+
+# number = 37568
+# n1 = number % 10
+# n2 = number % 100 // 10
+# n3 = number // 100 % 10
+# n4 = number // 1000 % 10
+# n5 = number // 10000
+# print(n1)
+# print(n2)
+# print(n3)
+# print(n4)
+# print(n5)
+# result = n1 + n2 + n3 + n4 + n5
+# print("Result:", result)
+
+#  ДЗ 1
+
+
+# number = 1234
+# n1 = number // 1000 % 10
+# n2 = number // 100 % 10
+# n3 = number % 100 // 10
+# n4 = number % 10
+# print(n1)
+# print(n2)
+# print(n3)
+# print(n4)
+# result = n1 + n2 + n3 + n4
+# print("Result:", result)
+
+
+
+#  ДЗ 2
+
+
+# n1 = int(input())
+#
+# n2 = 0
+#
+# while n1 > 0:
+#     digit = n1 % 10
+#     n1 = n1 // 10
+#
+#     n2 = n2 * 10 + digit
+#
+# print(n2)
+
+# number = int(input('Enter a number:'))
+# n1 = 1
+# n2 = 2
+# n3 = 3
+# n4 = 4
+# n5 = 5
+# result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
+# print(result)
+
+
+# number = int(input('Enter a number:'))
+# n1 = 3
+# n2 = 7
+# n3 = 5
+# n4 = 6
+# n5 = 8
+# result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
+# print(result)
