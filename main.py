85fc6a7802a52a23c502606e22ee0eaf25801014
SerhiiNahradskyi <djseryoga3@gmail.com>
2024-10-15 19:58:48 +0200

    homework task-2

--- main.py
+++ main.py
@@ -68,3 +68,68 @@
 # n5 = 8
 # result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
 # print(result)
+
+# n1, n2 = 10, 20
+# print(n1 > n2)
+# print(n1 >= n2)
+# print(n1 < n2)
+# print(n1 <= n2)
+# print(n1 == n2)
+# print(n1 != n2)
+# result = n1 == n2
+
+# print(1 == 1 and 3 == 3)
+# print(1 == 1 or 2 == 3)
+# is_valid = False
+# print(not is_valid)
+# print("Hello" in "Hello world")
+#
+# hours = int(input("Enter hours:"))
+# if hours >= 12:
+#     print("PN")
+# else:
+#     print("AM")
+
+
+
+# number1 = int(input("Enter first number:"))
+# number2 = int(input("Enter first number:"))
+# number3 = int(input("Enter first number:"))
+# if number1 <= number2 <= number3:
+#     print(number1)
+# elif number2 <= number1 <= number3:
+#     print(number2)
+# elif number3 <= number2 <= number1:
+#     print(number3)
+
+
+# number1 = int(input("Enter first number:"))
+# number2 = int(input("Enter first number:"))
+# number3 = int(input("Enter first number:"))
+#
+# if number1 == number2 == number3:
+#     print(3)
+# elif number1 == number2 or number1 == number3 or number2 == number1:
+#     print()
+
+
+
+# user_select = int(input("1. Start\n2. Settings\n3. Saved games\n4. Exit\nSelect"))
+
+
+# match user_select:
+#     case 1:
+#         print("Starting game...")
+#     case 2:
+#         print("Settings")
+#     case 3:
+#         print("Saved games")
+#     case 3:
+#         print("Exit")
+#     case _:
+#         print("Invalid input please try again")
+
+# калькулятор
+# while True: print(eval(input(">>>")))
+
+
