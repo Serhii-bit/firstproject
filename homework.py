6c850bca20230edc4b6238e4c4a0b3772a700925
SerhiiNahradskyi <djseryoga3@gmail.com>
2024-10-18 20:34:58 +0200

    homework task-3

---
+++ homework.py
@@ -0,0 +1,105 @@
+########### ДЗ 3.1
+# v1
+#
+# while True: print(eval(input(">>>")))
+
+# v2
+
+# print("Оберіть операцію.")
+# print("1. +")
+# print("2. -")
+# print("3. *")
+# print("4. /")
+# choice = input("Введіть число операції (1,2,3,4):")
+# num1 = float(input("Введіть перше число:"))
+# num2 = float(input("Введіть друге число:"))
+#
+# if choice == "1":
+#     print(num1, "+", num2,"=", num1+num2)
+# elif choice == "2":
+#     print(num1, "-", num2,"=", num1-num2)
+# elif choice == "3":
+#     print(num1, "*", num2, "=", num1*num2)
+# elif choice == "4":
+#     print(num1, "/", num2, "=", num1/num2)
+# else:
+#     print("Невірна операція!")
+# input("Нажміть Enter для виходу.")
+
+########### ДЗ 3.2
+
+#
+
+# number = [12, 3, 4, 10]
+# number.insert(0, number[-1])
+# number.pop()
+# print(number)
+
+#
+
+# number = [12, 3, 4, 10, 8]
+# number.insert(0, number[-1])
+# number.pop()
+# print(number)
+
+#
+
+# number = [1]
+# number.insert(0, number[-1])
+# number.pop()
+# print(number)
+
+#
+
+# nums = []
+# nums.insert(0,nums)
+# nums.pop()
+# print(nums)
+
+#
+
+########### ДЗ 3.3
+
+
+# number = [1, 2, 3, 4, 5, 6]
+# part1 = number[:len(number)//2]
+# part2 = number[len(number)//2:]
+# result = [part1, part2]
+# print(result)
+
+#
+
+# number = [1, 2, 3]
+# part1 = number[:len(number)//2+1]
+# part2 = number[len(number)//2-2:]
+# result = [part1, part2]
+# print(result)
+
+# part1 = number[len(number)//2:]
+# part2 = number[:len(number)//2]
+#
+
+# number = [1, 2, 3, 4, 5]
+# part1 = number[:len(number)//2+1]
+# part2 = number[len(number)//1-2:]
+# result = [part1, part2]
+# print(result)
+
+#
+
+# number = [1]
+# part1 = number[len(number)//1-2:]
+# part2 = number[:len(number)//2]
+# result = [part1, part2]
+# print(result)
+
+#
+
+# nums = []
+# part1 = nums[len(nums)//1-2:]
+# part2 = nums[:len(nums)//2]
+# result = [part1, part2]
+# print(result)
+
+
+
